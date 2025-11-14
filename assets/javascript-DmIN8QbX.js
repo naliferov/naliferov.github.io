369cca5a313bb20631c79d841199e17302fc69d3
Nick Aliferov <x8core@gmail.com>
2025-11-14 18:35:08 +0200

    Updates

--- assets/javascript-DmIN8QbX.js
+++ assets/javascript-DmIN8QbX.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-UebaDczz.js";import"./x-XoHX3BFZ.js";import"./index-DeYumv67.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-AEsLK5le.js";import"./x-DMCQ9Iqs.js";import"./index-DQGWlJoM.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.54.0(7c2310116c57517348bbd868a21139f32454be22)
  * Released under the MIT license
--- assets/razor-CX9ogAGq.js
+++ assets/razor-CX9ogAGq.js
@@ -1,4 +1,4 @@
-import{m}from"./x-XoHX3BFZ.js";import"./index-DeYumv67.js";/*!-----------------------------------------------------------------------------
+import{m}from"./x-DMCQ9Iqs.js";import"./index-DQGWlJoM.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.54.0(7c2310116c57517348bbd868a21139f32454be22)
  * Released under the MIT license
